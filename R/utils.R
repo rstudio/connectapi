837bd002f549353aa4f62015cce8922e7e80be04
Cole Arendt <cole.arendt@outlook.com>
2019-01-19 09:33:13 -0500

    rename safe_query and add option for multiples

--- R/utils.R
+++ R/utils.R
@@ -1,7 +1,7 @@
-safequery <- function(expr, prefix = "") {
+safe_query <- function(expr, prefix = "", collapse = "|") {
   if (is.null(expr)) {
     return("")
   } else {
-    return(paste0(prefix, expr))
+    return(paste0(prefix, glue::glue_collapse(expr, sep = collapse)))
   }
 }
