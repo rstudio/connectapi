c924e00d6895a502682d471291b651378e3ef005
Cole Arendt <cole.arendt@outlook.com>
2019-01-21 15:14:53 -0500

    add download_github helper

---
+++ R/github.R
@@ -0,0 +1,24 @@
+#' @export
+download_github <- function(repo, ref = "master") {
+  current_wd <- getwd()
+  on.exit(setwd(current_wd), add = TRUE)
+  
+  temp_dir <- fs::dir_create(fs::path(fs::path_temp(), fs::file_temp("dir_")), recursive = TRUE)
+  tar_file <- fs::path(temp_dir, "repo.tar.gz")
+  setwd(temp_dir)
+  
+  req <- httr::GET(
+    glue::glue(
+      "https://api.github.com/repos/{repo}/tarball/{ref}"
+    ), 
+    httr::write_disk(tar_file)
+  )
+  
+  # un-tar and enter the repo
+  untar("repo.tar.gz")
+  setwd(fs::dir_ls(type = "directory"))
+  
+  final_loc <- getwd()
+  
+  return(final_loc)
+}
