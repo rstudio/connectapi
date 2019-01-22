dc227c92e0a6fd38a935bca62a4d443153403b2c
Cole Arendt <cole.arendt@outlook.com>
2019-01-21 22:10:25 -0500

    add some work to the yaml functions

--- R/yaml.R
+++ R/yaml.R
@@ -1,25 +1,50 @@
 yaml_content <- function(filename = ".connect.yml") {
   cfg <- config::get(value = "content", file = filename)
+  return(cfg)
 }
 
 yaml_content_deploy <- function(
+  connect,
   name,
   path = "./",
   description = NULL,
   tag = NULL,
   url = NULL,
-  image = NULL
+  image = NULL,
+  ...
 ) {
   bundle_path <- dir_bundle(path = path)
   
-  # create_and_upload
+  c_obj <- rlang::exec(
+    content_ensure,
+    connect = connect,
+    name = name,
+    description = description
+    ...
+  )
   
-  # should have a guid
+  c_guid <- c_obj[["guid"]]
+  
+  c_upload <- connect$content_upload(
+    bundle_path = bundle_path,
+    guid = c_guid
+    )
+  
+  # deploy
+  
   
   # tag helper
+  if (!is.null(tag)) {
+    
+  }
   
   # set vanity URL
+  if (!is.null(url)) {
+    
+  }
   
   # set image path
-  
+  if (!is.null(image)) {
+    
+  }
 }
