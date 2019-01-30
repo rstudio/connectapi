de83a546e49b6ebbb344d28ef5627f441908ce36
Cole Arendt <cole.arendt@outlook.com>
2019-01-30 09:02:53 -0500

    update deployment tests

--- tests/testthat/integrated-tests/test-deployment.R
+++ tests/testthat/integrated-tests/test-deployment.R
@@ -7,6 +7,7 @@ test_conn_2 <- Connect$new(host = Sys.getenv("TEST_SERVER_2"), api_key = Sys.get
 cont1_name <- uuid::UUIDgenerate()
 cont1_title <- "Test Content 1"
 cont1_guid <- NULL
+cont1_bundle <- NULL
 
 test_that("can create content", {
   cont1 <- test_conn_1$content_create(name = cont1_name, title = cont1_title)
@@ -19,13 +20,13 @@ test_that("can create content", {
 })
 
 test_that("can upload and deploy content", {
-  bundle <- dir_bundle(
+  cont1_bundle <<- dir_bundle(
       rprojroot::find_testthat_root_file("test-plot"), 
       "../test-ex-1.tar.gz"
   )
-  expect_true(fs::file_exists(bundle))
+  expect_true(fs::file_exists(cont1_bundle))
   
-  res <- test_conn_1$content_upload(bundle_path = bundle, guid = cont1_guid)
+  res <- test_conn_1$content_upload(bundle_path = cont1_bundle, guid = cont1_guid)
   expect_false(is.null(res))
   expect_silent(as.integer(res[["bundle_id"]]))
   
@@ -53,4 +54,40 @@ test_that("can promote content to another server", {
   )
   
   expect_identical(cont1_name, cont1_2[["name"]])
+})
+
+test_that("content_ensure works with guid", {
+  c1 <- content_ensure(test_conn_1, guid = cont1_guid)
+  expect_identical(c1[["guid"]], cont1_guid)
+  
+  fake_guid <- paste0(cont1_guid, "-does-not-exist")
+  expect_warning({c2 <- content_ensure(test_conn_1, guid = fake_guid)})
+  expect_false(identical(c2[["guid"]], cont1_guid))
+})
+
+test_that("content_ensure works with name", {
+  expect_message(c_new <- content_ensure(test_conn_1))
+  expect_is(c_new[["guid"]], "character")
+  
+  expect_message(
+    c_same <- content_ensure(test_conn_1, name = c_new[["name"]])
+  )
+  
+  expect_identical(c_new[["name"]], c_same[["name"]])
+  expect_identical(c_new[["guid"]], c_same[["guid"]])
+  
+  c_newname <- paste0(c_new[["name"]], "-alternate")
+  c_title <- "Some Title"
+  c_desc <- "Some Description"
+  expect_message(
+    c_diff <- content_ensure(test_conn_1, name = c_newname, 
+                             title = c_title, description = c_desc)
+  )
+  
+  expect_false(identical(c_new[["name"]], c_diff[["name"]]))
+  expect_false(identical(c_new[["guid"]], c_diff[["guid"]]))
+  expect_identical(c_newname, c_diff[["name"]])
+  expect_identical(c_title, c_diff[["title"]])
+  expect_identical(c_desc, c_diff[["description"]])
+  
 })
