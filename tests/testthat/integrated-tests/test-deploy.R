66a9cddd06bc96907bf8951f6e393496c66e7196
Cole Arendt <cole.arendt@outlook.com>
2019-02-27 21:48:46 -0500

    add bundle_dir testing

--- tests/testthat/integrated-tests/test-deploy.R
+++ tests/testthat/integrated-tests/test-deploy.R
@@ -10,7 +10,18 @@ cont1_guid <- NULL
 cont1_bundle <- NULL
 
 test_that("bundle_dir deploys", {
-  skip("not implemented yet")
+  dir_path <- rprojroot::find_testthat_root_file("examples/static")
+  tmp_file <- fs::file_temp(pattern = "bundle", ext = ".tar.gz")
+  bund <- bundle_dir(test_conn_1, path = dir_path, filename = tmp_file)
+  
+  expect_equal(tmp_file, bund$path)
+  
+  tsk <- deploy(bundle = bund, name = cont1_name, title = cont1_title)
+  
+  cont1_guid <<- tsk$get_content()$guid
+  
+  expect_equal(tsk$get_content()$name, cont1_name)
+  expect_equal(tsk$get_content()$title, cont1_title)
 })
 
 test_that("bundle_path deploys", {
