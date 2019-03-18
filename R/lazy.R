f4ee0bc0daad818574324fe38a263179ea2f747b
Cole Arendt <cole.arendt@outlook.com>
2019-03-17 22:42:56 -0400

    make n functional

--- R/lazy.R
+++ R/lazy.R
@@ -6,6 +6,7 @@
 # - draw diagram for understanding dbplyr execution
 # - how does the op-list work... can you make "collect" happen before filter, mutate, and such?
 # - some type of quoting here...?
+#' @export
 tbl_connect <- function(src, from, ...) {
   vars <- "test"
   
@@ -14,8 +15,10 @@ tbl_connect <- function(src, from, ...) {
   dplyr::make_tbl(c("connect", "lazy"), src = src, ops = ops)
 }
 
-collect.tbl_connect <- function(x, ...) {
-  res <- x$src$get_users()
+#' @importFrom dplyr collect
+#' @export
+collect.tbl_connect <- function(x, ..., n = Inf) {
+  res <- x$src$get_users(page_size = n)
   purrr::map_df(res$results, tibble::as_tibble)
 }
 
@@ -23,12 +26,30 @@ cat_line <- function(...) {
   cat(paste0(..., "\n"), sep = "")
 }
 
+#' @importFrom utils head
+#' @export
+head.tbl_connect <- function(x, n = 6L, ...) {
+  if (inherits(x$ops, "op_head")) {
+    x$ops$args$n <- min(x$ops$args$n, n)
+  } else {
+    x$ops <- op_single("head", x = x$ops, args = list(n = n))
+  }
+  x
+}
+
+#' @export
 print.tbl_connect <- function(x, ..., n = NULL) {
   cat_line(format(x, ..., n = n))
   invisible(x)
 }
 
+#' @export
 as.data.frame.tbl_connect <- function(x, row.names = NULL, optional = NULL, ..., n = Inf) {
+  print(capture.output(str(x$ops)))
+  # handle "head" manually for now
+  if (x$ops$name == "head") {
+    n = x$ops$args$n
+  }
   as.data.frame(collect(x, n = n))
 }
 
@@ -47,3 +68,16 @@ op_base <- function(x, vars, class = character()) {
     class = c(paste0("op_base_", class), "op_base", "op")
   )
 }
+
+op_single <- function(name, x, dots = list(), args = list()) {
+  structure(
+    list(
+      name = name,
+      x = x,
+      dots = dots,
+      args = args
+    ),
+    class = c(paste0("op_", name), "op_single", "op")
+  )
+}
+
