e94b906fb073cce4b50c0fe1d1a933d4043347ef
Cole Arendt <cole.arendt@outlook.com>
2019-05-30 23:39:20 -0400

    add explanatory note to test file

--- tests/testthat/test-print.R
+++ tests/testthat/test-print.R
@@ -1,5 +1,9 @@
 context("test printing")
 
+# this (nonstandard) test checks output against a file
+# thus allowing an easy preview of expected output
+# AND some level of standard / consistency / expectation
+
 # regenerate test printing file with the following
 # writeLines(connectapi:::generate_R6_print_output(), rprojroot::find_testthat_root_file("test-print-output.txt"))
 
