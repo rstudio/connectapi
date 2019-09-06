932c31c432771beecd7312a27df3e6124c1d2692
Cole Arendt <cole.arendt@outlook.com>
2019-09-06 00:03:48 -0400

    clean up paging limit (for small requests)

--- R/page.R
+++ R/page.R
@@ -1,9 +1,9 @@
 #' @export
-page_cursor <- function(client, req) {
+page_cursor <- function(client, req, limit = Inf) {
   qreq <- rlang::enquo(req)
   
   prg <- progress::progress_bar$new(
-    format = "downloading page :current (:tick_rate/sec)",
+    format = "downloading page :current (:tick_rate/sec) :elapsedfull",
     total = NA,
     clear = FALSE
   )
@@ -12,7 +12,7 @@ page_cursor <- function(client, req) {
   response <- rlang::eval_tidy(qreq)
   
   res <- response$results
-  while(!is.null(response$paging$`next`)) {
+  while(!is.null(response$paging$`next`) && length(res) < limit) {
     prg$tick()
     response <- client$GET_URL(response$paging$`next`)
     res <- c(res, response$results)
