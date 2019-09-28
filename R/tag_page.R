12f2c161e06592234b5cb3919a316ef12b50ccda
Cole Arendt <cole.arendt@outlook.com>
2019-09-27 22:35:01 -0400

    document tag_page_iframe add more formal metadata

--- R/tag_page.R
+++ R/tag_page.R
@@ -4,7 +4,7 @@
 #' priveleges to that content.
 #'
 #' @param connect A Connect object
-#' @param tag The name of the targeted tag
+#' @param tag The name of the targeted tag. NOTE: tag names are not necessarily unique
 #' @param description A description of the tag, placed at the top of the landing
 #'   page
 #'
@@ -68,14 +68,33 @@ take_screenshot <- function(app, tag, server_key) {
   fname
 }
 
-# unfortunately iframes do not seem to pass cookies cleanly...
-# i can't even get this to work in the "same-origin" setup (where the iframe uses a different origin from the parent)
-# https://security.stackexchange.com/questions/182518/how-to-confirm-that-an-embedded-iframe-can-read-cookies-from-parent
-# https://www.html5rocks.com/en/tutorials/security/sandboxed-iframes/
-# https://stackoverflow.com/questions/31184505/sandboxing-iframe-and-allow-same-origin
-# https://stackoverflow.com/questions/2117248/setting-cookie-in-iframe-different-domain
-# https://stackoverflow.com/questions/13432821/is-it-possible-to-add-request-headers-to-an-iframe-src-request
-tag_page_iframe <- function(connect, tag) {
+#' Tag Page iframe
+#' 
+#' Unfortunately, iframes do not pass cookies as cleanly as I would like... I am having
+#' trouble even getting this to work in the "same-origin" setup that _should_ work (if
+#' the iframe uses a different origin from the parent)
+#' 
+#' @references
+#' \itemize{
+#'   \item{\url{https://security.stackexchange.com/questions/182518/how-to-confirm-that-an-embedded-iframe-can-read-cookies-from-parent}}
+#'   \item{\url{https://www.html5rocks.com/en/tutorials/security/sandboxed-iframes/}}
+#'   \item{\url{https://stackoverflow.com/questions/31184505/sandboxing-iframe-and-allow-same-origin}}
+#'   \item{\url{https://stackoverflow.com/questions/2117248/setting-cookie-in-iframe-different-domain}}
+#'   \item{\url{https://stackoverflow.com/questions/13432821/is-it-possible-to-add-request-headers-to-an-iframe-src-request}}
+#' }
+#' 
+#' @param connect A Connect object
+#' @param tag A tag to search for. NOTE: tag names are not necessarily unique
+#' @param metadata A metadata object to be used for enriching application data
+#' 
+#' @examples 
+#' app_metadata <- list(
+#'   list(guid = "abcde-abcd-abcd-abcde", content_group = "Group 1"),
+#'   list(guid = "fghij-fghi-fghi-fghij", content_group = "Group 2")
+#' )
+#' 
+#' @keywords internal
+tag_page_iframe <- function(connect, tag, metadata = NULL) {
   client <- connect
   tag_id <- client$get_tag_id(tag)
   apps <- client$get_apps(filter = list(tag = as.character(tag_id)))
@@ -84,15 +103,8 @@ tag_page_iframe <- function(connect, tag) {
     stop(sprintf('No applications found on %s matching tag %s', server, tag))
   }
   
-  # for prototyping
-  if (length(apps) > 2) {
-    apps[[1]]$content_group <- "Group 1"
-    apps[[2]]$content_group <- "Group 1"
-  }
-  if (length(apps) > 5) {
-    apps[[4]]$content_group <- "Group 2"
-    apps[[5]]$content_group <- "Group 2"
-  }
+  # set metadata (only content_group is used right now)
+  purrr::reduce(metadata, apply_metadata, .init = apps)
 
   template <- system.file('tag_page_iframe.Rmd', package = "connectapi")
   
@@ -113,4 +125,14 @@ tag_page_iframe <- function(connect, tag) {
     LANDING_PAGE  = normalizePath(sprintf('%s/%s', out_dir, out_file)),
     APPS = apps
   )
+}
+
+# helper function to set app metadata (i.e. content_group)
+apply_metadata <- function(all_apps, single_metadata) {
+  which_match <- purrr::map_lgl(all_apps, ~ .x$guid == single_metadata$guid)
+  all_apps[which_match] <- purrr::map(
+    all_apps[which_match], 
+    ~ purrr::list_modify(.x, !!!single_metadata)
+    )
+  return(all_apps)
 }
