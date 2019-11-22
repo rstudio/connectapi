8d6de07d7d935c00a19a03273e11ff5256970144
Cole Arendt <cole.arendt@outlook.com>
2019-11-22 08:27:08 -0500

    add a few TODO items

--- R/migrate-hackery.R
+++ R/migrate-hackery.R
@@ -5,6 +5,8 @@ deploy_current <- function(content) {
 }
 
 # ACLs ----------------------------------------------------
+
+# TODO: How should we behave if this content is open to the server / world?
 get_acl <- function(content) {
   warn_experimental("get_acl")
   client <- content$get_connect()
@@ -36,6 +38,7 @@ acl_add_collaborator <- function(content, user_guid) {
 }
 
 # TODO: Should this be a warning if the user is a collaborator? Will downgrade their permissions
+# TODO: How should this behave if the content does not have access_type: acl?
 acl_add_viewer <- function(content, user_guid) {
   acl_add_user(content = content, user_guid = user_guid, role = "viewer")
 }
