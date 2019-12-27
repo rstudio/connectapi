4d1cb92ca896dcd0272bf262278667a31b00ce32
Cole Arendt <cole.arendt@outlook.com>
2019-12-27 11:50:48 -0500

    add simple get test for procs

---
+++ tests/testthat/integrated-tests/test-get.R
@@ -0,0 +1,27 @@
+context("get")
+
+# should connect with env vars
+test_conn_1 <- connect(prefix = "TEST_1")
+test_conn_2 <- connect(prefix = "TEST_2")
+
+cont1_name <- uuid::UUIDgenerate()
+cont1_title <- "Test Content 1"
+cont1_guid <- NULL
+cont1_bundle <- NULL
+cont1_content <- NULL
+
+test_that("get_procs works", {
+  proc_data <- get_procs(test_conn_1)
+  
+  # TODO: This is not a great test, since no processes are running
+  # we could always start a content restoration...
+  expect_is(proc_data, "tbl_df")
+  expect_identical(
+    names(proc_data),
+    names(type_vector_proc)
+  )
+  expect_identical(
+    purrr::map(proc_data, typeof),
+    purrr::map(type_vector_proc, typeof)
+  )
+})
