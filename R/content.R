320a992b05db275c4ea10a5bd6cb62302c31e8fc
Cole Arendt <cole.arendt@outlook.com>
2020-01-11 19:19:27 -0500

    move the Content object to content.R

--- R/content.R
+++ R/content.R
@@ -1,3 +1,66 @@
+#' Content
+#' 
+#' An R6 class that represents content
+#' 
+#' @family deployR6 functions
+#' @export
+Content <- R6::R6Class(
+  "Content",
+  public = list(
+    #' @field connect An R6 Connect object
+    connect = NULL,
+    #' @field content The content details from RStudio Connect
+    content = NULL,
+    
+    initialize = function(connect, content) {
+      validate_R6_class(connect, "Connect")
+      self$connect <- connect
+      # TODO: need to check that content has
+      # at least guid, url, title to be functional
+      self$content <- content
+    },
+    get_connect = function(){self$connect},
+    get_content = function(){self$content},
+    get_content_remote = function(){
+      new_content_details <- self$get_connect()$content(self$get_content()$guid)
+      self$content <- new_content_details
+      self$get_content()
+    },
+    update = function(...) {
+      params <- rlang::list2(...)
+      url <- glue::glue("v1/experimental/content/{self$get_content()$guid}")
+      res <- self$get_connect()$POST(
+        url,
+        params
+      )
+      return(self)
+    },
+    get_dashboard_url = function(pane = ""){
+      dashboard_url_chr(self$connect$host, self$content$guid, pane = pane)
+    },
+    get_jobs = function() {
+      warn_experimental("get_jobs")
+      url <- glue::glue("applications/{self$get_content()$guid}/jobs")
+      self$get_connect()$GET(url)
+    },
+    get_job = function(key) {
+      warn_experimental("get_job")
+      url <- glue::glue("applications/{self$get_content()$guid}/job/{key}")
+      self$get_connect()$GET(url)
+    },
+    print = function(...) {
+      cat("RStudio Connect Content: \n")
+      cat("  Content GUID: ", self$get_content()$guid, "\n", sep = "")
+      cat("  Content URL: ", self$get_content()$url, "\n", sep = "")
+      cat("  Content Title: ", self$get_content()$title, "\n", sep = "")
+      cat("\n")
+      cat('content_item(client, guid = "', self$get_content()$guid, '")', "\n", sep = "")
+      cat("\n")
+      invisible(self)
+    }
+  )
+)
+
 #' Get ACL Details
 #' 
 #' \lifecycle{experimental} Retrieve the Access Controls
