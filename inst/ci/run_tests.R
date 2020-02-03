4352f487eef11e165ff270b903d2c255c724778f
Cole Arendt <cole.arendt@outlook.com>
2020-02-03 07:00:10 -0500

    make run_tests.R work in a clean environment

--- inst/ci/run_tests.R
+++ inst/ci/run_tests.R
@@ -1,23 +1,30 @@
+options("repos" = c(RSPM = "https://demo.rstudiopm.com/cran/__linux__/bionic/latest"))
+install.packages("devtools")
+
+message("installing dependencies")
+setwd("/test")
+devtools::install_deps(dep = TRUE)
+
 library(testthat)
-library(connectapi)
+devtools::load_all()
 
-hosts <- compose_find_hosts(prefix = "ci_connect")
-
+message("creating reporters")
 progress_reporter <- ProgressReporter$new(max_failures = 1000)
 junit_reporter <- JunitReporter$new(file = fs::path("test-results-junit.xml"))
 check_reporter <- CheckReporter$new(file = fs::path("test-results-check.txt"))
-rstudio_reporter <- RstudioReporter$new(file = fs::path("test-results-rstudio.txt"))
-multi_reporter <- MultiReporter$new(reporters = list(progress_reporter, junit_reporter, check_reporter, rstudio_reporter))
+multi_reporter <- MultiReporter$new(reporters = list(progress_reporter, junit_reporter, check_reporter))
 
+message("creating first admins")
 a1 <- create_first_admin(
-  hosts[[1]],
+  "http://connect1:3939",
   "admin", "admin0", "admin@example.com"
 )
 a2 <- create_first_admin(
-  hosts[[2]],
+  "http://connect2:3939",
   "admin", "admin0", "admin@example.com"
 )
 
+message("defining variables")
 Sys.setenv(
   "TEST_1_SERVER" = a1$host,
   "TEST_1_API_KEY" = a1$api_key,
@@ -25,4 +32,5 @@ Sys.setenv(
   "TEST_2_API_KEY" = a2$api_key
   )
 
+message("running integration tests")
 testthat::test_dir(path = "tests/testthat/integrated-tests/", reporter = multi_reporter, stop_on_failure = FALSE)
