fe94307362e77354f74acbe08bdfa2d9940f1d53
Cole Arendt <cole.arendt@outlook.com>
2020-02-04 09:31:24 -0500

    add the items removed by merge

--- R/content.R
+++ R/content.R
@@ -48,6 +48,66 @@ Content <- R6::R6Class(
       url <- glue::glue("applications/{self$get_content()$guid}/job/{key}")
       self$get_connect()$GET(url)
     },
+    jobs = function() {
+      warn_experimental("jobs")
+      url <- glue::glue("applications/{self$get_content()$guid}/jobs")
+      self$get_connect()$GET(url)
+    },
+    job = function(key) {
+      warn_experimental("job")
+      url <- glue::glue("applications/{self$get_content()$guid}/job/{key}")
+      self$get_connect()$GET(url)
+    },
+    variants = function() {
+      warn_experimental("variants")
+      url <- glue::glue("applications/{self$get_content()$guid}/variants")
+      self$get_connect()$GET(url)
+    },
+    tag_set = function(id) {
+      warn_experimental("tag_set")
+      url <- glue::glue("applications/{self$get_content()$guid}/tags")
+      self$get_connect()$POST(
+        url,
+        body = list(
+          id = id
+        )
+      )
+    },
+    tag_delete = function(id) {
+      # note that deleting the parent tag deletes all children
+      warn_experimental("tag_delete")
+      url <- glue::glue("applications/{self$get_content()$guid}/tags/{id}")
+      invisible(self$get_connect()$DELETE(url))
+    },
+    tags = function() {
+      warn_experimental("tags")
+      url <- glue::glue("applications/{self$get_content()$guid}/tags")
+      self$get_connect()$GET(url)
+    },
+    environment = function() {
+      warn_experimental("environment")
+      url <- glue::glue("applications/{self$get_content()$guid}/environment")
+      self$get_connect()$GET(url)
+    },
+    environment_set = function(..., .version = 0) {
+      warn_experimental("environment_set")
+      url <- glue::glue("applications/{self$get_content()$guid}/environment")
+      # post with 
+      # key = NA to retain
+      # post without a variable/key to remove
+      # bump version number each time
+      vals <- rlang::list2(...)
+      body <- list(
+        values = vals,
+        version = .version,
+        app_guid = self$get_content()$guid
+      )
+      self$get_connect()$POST(
+        path = url,
+        body = body
+      )
+      invisible()
+    },
     print = function(...) {
       cat("RStudio Connect Content: \n")
       cat("  Content GUID: ", self$get_content()$guid, "\n", sep = "")
@@ -57,6 +117,50 @@ Content <- R6::R6Class(
       cat('content_item(client, guid = "', self$get_content()$guid, '")', "\n", sep = "")
       cat("\n")
       invisible(self)
+    }
+  )
+)
+
+#' Variant
+#' 
+#' An R6 class that represents a Variant
+#' 
+Variant <- R6::R6Class(
+  "Variant",
+  inherit = Content,
+  public = list(
+    variant = NULL,
+    get_variant = function() {self$variant},
+    initialize = function(connect, content, variant) {
+      # TODO
+    },
+    send_mail = function(to = c("me", "collaborators", "collaborators_viewers")) {
+      if (length(to) > 1) to <- "me"
+      url <- glue::glue("variants/{self$get_variant()$id}/sender")
+      self$get_connect()$POST(
+        path = url, 
+        body = list(
+          email = to
+        ))
+    },
+    render = function() {
+      url <- glue::glue("variants/{self$get_variant()$id}/render")
+      self$get_connect()$POST(
+        path = url,
+        body = list(
+          email = "none",
+          activate = TRUE
+        )
+      )
+    },
+    get_renderings = function() {
+      url <- glue::glue("variants/{self$get_variant()$id}/renderings")
+      self$get_connect()$GET(
+        path = url
+      )
+    },
+    navigate_rev = function() {
+      glue::glue("content_url/variant_hash/_rev{rev_id}")
     }
   )
 )
