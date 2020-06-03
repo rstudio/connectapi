a71a99dc67c7ed65e5209085052695c06fb1b20b
Cole Arendt <cole.arendt@outlook.com>
2020-06-03 07:18:02 -0400

    iterate on schedule helpers

--- R/schedule.R
+++ R/schedule.R
@@ -55,17 +55,42 @@ get_variant_schedule <- function(variant) {
   VariantSchedule$new(connect = connect_client, content = content_details, key = variant_key, schedule = variant_schedule)
 }
 
+#' @param .schedule A schedule object. As returned by `get_variant_schedule()`
+#' @param type The type of schedule. One of "hour", "minute", "day", "weekday",
+#'   "week", "semimonth", "dayofmonth", "dayweekofmonth", "year"
+#' @param email Whether to send emails on this schedule
+#' @param activate Whether to publish the output of this schedule
+#' @param schedule A JSON blob (as a string) describing the schedule. See "More Details"
+#' @param start_time The start time of the schedule
+#' @param next_run The next run of the schedule (set to `start_time` in most cases)
 set_schedule <- function(
-  schedule, 
-  type = c(),
-  iter = c(),
-  start_time = Sys.time(),
-  publish = TRUE,
-  email = FALSE
+  .schedule, 
+  ...
   ) {
-  # TODO: set a schedule
+  validate_R6_class(.schedule, "VariantSchedule")
+  cli <- .schedule$get_connect()
+  path <- glue::glue("schedules/{.schedule$get_schedule()$id}")
+  params <- rlang::list2(...)
+  if ("schedule" %in% names(params)) {
+    orig_schedule <- params$schedule
+    if (is.list(params$schedule)) {
+      params$schedule <- jsonlite::toJSON(params$schedule, auto_unbox = TRUE)
+    }
+    if (!(is.character(params$schedule) && length(params$schedule) == 1 && jsonlite::validate(params$schedule))) {
+      stop(glue::glue("The schedule you provided is invalid: {capture.output(str(orig_schedule))}"))
+    }
+  }
+  final_params <- purrr::list_modify(.schedule$get_schedule_remote(), !!!params)
+  res <- cli$POST(path = path, body = final_params)
+  
+  .schedule$get_schedule_remote()
+  .schedule
 }
 
-set_schedule_remove <- function(schedule) {
-  # TODO: delete a schedule
+set_schedule_remove <- function(.schedule) {
+  validate_R6_class(.schedule, "VariantSchedule")
+  cli <- .schedule$get_connect()
+  path <- glue::glue("schedules/{.schedule$get_schedule()$id}")
+  cli$DELETE(path = path)
+  get_variant(.schedule, .schedule$key)
 }
