a5b383cca630372136f3a701ba01fa63a22e18ce
Cole Arendt <cole.arendt@outlook.com>
2021-05-21 17:37:44 +0200

    fix more host references

--- tests/integrated/test-zzaudit.R
+++ tests/integrated/test-zzaudit.R
@@ -12,7 +12,7 @@ cont1_bundle <- NULL
 test_that("basic behavior works", {
   all_apps <- cache_apps(test_conn_1)
 
-  res <- audit_vanity_urls(all_apps, test_conn_1$host)
+  res <- audit_vanity_urls(all_apps, test_conn_1$server)
   expect_is(res, "character")
 
   # does not work today (because R version needed)
