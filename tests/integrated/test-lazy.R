cc1ab9a20f5cb667e8113bf41be011563b7da1cb
Cole Arendt <cole.arendt@outlook.com>
2021-11-10 22:36:17 +0100

    protect against empty content list https://github.com/rstudio/connect/issues/20233

--- tests/integrated/test-lazy.R
+++ tests/integrated/test-lazy.R
@@ -10,6 +10,9 @@ cont1_guid <- NULL
 cont1_bundle <- NULL
 cont1_content <- NULL
 
+bnd_static <- bundle_dir(rprojroot::find_testthat_root_file("test-static"))
+tmp_content <- deploy(test_conn_1, bnd_static)
+
 test_that("error on bad 'src' object", {
   expect_error(
     tbl_connect("bad_src", "users"),
