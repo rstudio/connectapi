66f59ac4ed10ed3842e5b444f9f1107807658911
Cole Arendt <cole.arendt@outlook.com>
2022-01-05 16:39:51 +0100

    fix tests for environment add more tests for environment

--- tests/integrated/test-content.R
+++ tests/integrated/test-content.R
@@ -148,51 +148,99 @@ test_that("update owner_guid works", {
   )
 })
 
+# Environment ------------------------------------
+
 test_that("get_environment works with no environment variables", {
-  scoped_experimental_silence()
   env <- get_environment(rmd_content)
   curr_vers <- env$env_version
   env <- get_environment(rmd_content)
 
-  expect_identical(env$env_vars, list(a = 1)[0])
-  expect_equal(env$env_version, curr_vers)
+  expect_identical(env$env_vars, list())
 })
 
 test_that("set_environment works", {
   scoped_experimental_silence()
   env <- get_environment(rmd_content)
-  curr_vers <- env$env_version
 
   new_env <- set_environment_new(env, test = "value", test1 = TRUE, test2 = 4567)
 
   expect_equal(
     new_env$env_vars,
     list(
-      test = NA_character_, test1 = NA_character_, test2 = NA_character_
+      "test", "test1", "test2"
       )
     )
-  expect_equal(new_env$env_version, curr_vers + 1)
 
   new_env1 <- set_environment_new(env, test1 = "another")
   expect_equal(
     new_env$env_vars,
     list(
-      test = NA_character_, test1 = NA_character_, test2 = NA_character_
+      "test", "test1", "test2"
     )
   )
 
   # remove a value multiple times
   rm1 <- set_environment_remove(env, test)
-  expect_equal(rm1$env_vars, list(test1 = NA_character_, test2 = NA_character_))
+  expect_equal(rm1$env_vars, list("test1", "test2"))
   rm2 <- set_environment_remove(env, test)
-  expect_equal(rm2$env_vars, list(test1 = NA_character_, test2 = NA_character_))
+  expect_equal(rm1$env_vars, list("test1", "test2"))
 
   rm3 <- set_environment_remove(env, "test1")
-  expect_equal(rm3$env_vars, list(test2 = NA_character_))
+  expect_equal(rm3$env_vars, list("test2"))
 
-  rm4 <- set_environment_remove(env, test2 = "hi")
-  expect_equal(rm4$env_vars, list(a = 1)[0])
+  rm4 <- set_environment_remove(env, "test2")
+  expect_equal(rm4$env_vars, list())
 })
+
+test_that("add environment variable works", {
+  res <- set_environment_new(rmd_content, MYVAR="hi")
+  expect_true("MYVAR" %in% res$env_vars)
+  expect_true(validate_R6_class(res, "Environment"))
+})
+
+test_that("edit environment variable works", {
+  res <- set_environment_new(rmd_content, MYVAR="hi", OTHER="other")
+  res2 <- set_environment_new(rmd_content, MYVAR="hi2")
+  expect_true("MYVAR" %in% res2$env_vars)
+  expect_true("OTHER" %in% res2$env_vars)
+  expect_true(validate_R6_class(res2, "Environment"))
+})
+
+test_that("get environment works", {
+  res <- set_environment_new(rmd_content, MYVAR="hi")
+  res <- get_environment(rmd_content)
+  expect_true("MYVAR" %in% res$env_vars)
+  expect_true(validate_R6_class(res, "Environment"))
+})
+
+test_that("remove environment variable works", {
+  res <- set_environment_new(rmd_content, MYVAR="hi", ANOTHER="how")
+  rem <- set_environment_new(rmd_content, MYVAR=NA)
+
+  expect_false("MYVAR" %in% rem$env_vars)
+  expect_true("ANOTHER" %in% rem$env_vars)
+  expect_true(validate_R6_class(res, "Environment"))
+})
+
+test_that("set all environment variables works", {
+  res <- set_environment_all(rmd_content)
+  expect_true(length(res$env_vars) == 0)
+  expect_true(validate_R6_class(res, "Environment"))
+
+  res <- set_environment_all(rmd_content, "MYVAR" = "ONE", "ANOTHER" = "HELLO")
+  expect_equal(res$env_vars, list("ANOTHER", "MYVAR"))
+  expect_true(validate_R6_class(res, "Environment"))
+
+  res <- set_environment_all(rmd_content, "MYVAR" = "TWO", "HELLO" = "AGAIN")
+  expect_equal(res$env_vars, list("HELLO", "MYVAR"))
+  expect_true(validate_R6_class(res, "Environment"))
+
+  res <- set_environment_all(rmd_content)
+  expect_true(length(res$env_vars) == 0)
+  expect_true(validate_R6_class(res, "Environment"))
+})
+
+# Bundles ---------------------------------------------------
 
 test_that("get_bundles and delete_bundle work", {
   bnd_name <- create_random_name()
@@ -796,3 +844,5 @@ test_that("remove a collaborator twice works", {
   })
   expect_false(any(which_match))
 })
+
+
