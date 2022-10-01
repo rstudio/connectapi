ef7323ce095e821d8b55dcf3b58efaac626104cc
Cole Arendt <cole.arendt@outlook.com>
2022-09-30 20:24:03 -0400

    add a CI helper to make toggling integrated tests easier

--- R/utils-ci.R
+++ R/utils-ci.R
@@ -3,6 +3,28 @@
 # TODO: A nicer way to execute these system commands...
 # - debug output... better error handling... etc.
 
+toggle_integrated <- function(to=NULL) {
+  env_var <- "CONNECTAPI_INTEGRATED"
+  curr_val <- FALSE
+  if (is.null(to)) {
+    env_val <- Sys.getenv(env_var)
+    if (tolower(env_val) == "true") {
+      curr_val <- TRUE
+    } else {
+      curr_val <- FALSE
+    }
+  } else {
+    curr_val <- !to
+  }
+
+  if (curr_val) {
+    do.call(Sys.setenv, as.list(rlang::set_names("false", env_var)))
+  } else {
+    do.call(Sys.setenv, as.list(rlang::set_names("true", env_var)))
+  }
+  return(!curr_val)
+}
+
 # set up test servers...
 find_compose <- function() {
   wh <- processx::process$new("which", "docker-compose", stdout = "|", stderr = "|")
