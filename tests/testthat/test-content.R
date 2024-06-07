55e8701f03acb39d89caac6461e6d81346015983
Neal Richardson <neal.p.richardson@gmail.com>
2024-06-07 10:56:55 -0400

    Add tests for environment var requests before I refactor more

--- tests/testthat/test-content.R
+++ tests/testthat/test-content.R
@@ -119,6 +119,36 @@ with_mock_api({
       "https://connect.example/__api__/v1/content/f2f37341-e21d-3d80-c698-a935ad614066/permissions"
     )
   })
+
+  test_that("content environment vars", {
+    con <- Connect$new(server = "https://connect.example", api_key = "fake")
+    item <- content_item(con, "f2f37341-e21d-3d80-c698-a935ad614066")
+
+    expect_GET(
+      item$environment(),
+      "https://connect.example/__api__/v1/content/f2f37341-e21d-3d80-c698-a935ad614066/environment"
+    )
+    expect_PATCH(
+      item$environment_set(VAR_NAME = "new_value"),
+      "https://connect.example/__api__/v1/content/f2f37341-e21d-3d80-c698-a935ad614066/environment",
+      '[{"name":"VAR_NAME","value":"new_value"}]'
+    )
+    expect_PATCH(
+      item$environment_set(VAR_NAME = NA),
+      "https://connect.example/__api__/v1/content/f2f37341-e21d-3d80-c698-a935ad614066/environment",
+      '[{"name":"VAR_NAME","value":null}]'
+    )
+    expect_PUT(
+      item$environment_all(VAR_NAME = "new_value"),
+      "https://connect.example/__api__/v1/content/f2f37341-e21d-3d80-c698-a935ad614066/environment",
+      '[{"name":"VAR_NAME","value":"new_value"}]'
+    )
+    expect_PUT(
+      item$environment_all(),
+      "https://connect.example/__api__/v1/content/f2f37341-e21d-3d80-c698-a935ad614066/environment",
+      "[]"
+    )
+  })
 })
 
 without_internet({
