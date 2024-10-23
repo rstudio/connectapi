1fb56f445707de70795332443777643d16996749
Toph Allen <toph.allen@gmail.com>
2024-10-23 11:10:12 -0400

    cleanup

--- tests/testthat/test-schedule.R
+++ tests/testthat/test-schedule.R
@@ -28,51 +28,6 @@ test_that("get_timezones() gets timeszones from v1 url when available", {
 })
 
 test_that("get_timezones() gets timeszones from unversioned url when v1 returns 404", {
-  mock_get_2024.08.0 <- function(url, parser = "parsed", ...) {
-    self$call_log(url)
-    if (url == "v1/timezones") {
-      res <- structure(
-        list(
-          url = "__api__/v1/timezones",
-          status_code = 404L,
-          request = structure(list(url = "__api__/v1/timezones"), class = "request"),
-          headers = structure(
-            list(
-              "content-type" = "text/plain"
-            ),
-            class = c("insensitive", "list")
-          ),
-          content = charToRaw("404 page not found")
-        ),
-        class = "response"
-      )
-    } else if (url == "timezones") {
-      res <- structure(
-        list(
-          url = "__api__/timezones",
-          status_code = 200L,
-          headers = structure(
-            list(
-              "content-type" = "application/json; charset=utf-8"
-            ),
-            class = c("insensitive", "list")
-          ),
-          content = charToRaw("[{\"timezone\":\"Africa/Abidjan\",\"offset\":\"+00:00\"},{\"timezone\":\"Africa/Accra\",\"offset\":\"+00:00\"}]")
-        ),
-        class = "response"
-      )
-    } else {
-      stop("Unexpected URL called")
-    }
-
-    if (is.null(parser)) {
-      res
-    } else {
-      self$raise_error(res)
-      httr::content(res, as = parser)
-    }
-  }
-
   client <- MockConnect$new()
 
   client$mock_response(
