a7c41b3e590863899089d1254375250b20f45e38
Toph Allen <toph.allen@gmail.com>
2024-10-29 19:15:17 -0400

    fix formatting snafu in docs

--- R/get.R
+++ R/get.R
@@ -708,11 +708,12 @@ get_oauth_credentials = function(connect, user_session_token) {
 #' Get a table showing available versions of R, Python, Quarto, and Tensorflow
 #' on the Connect server.
 #' 
-#' @param client A `Connect` object. @param runtimes Optional. A character
-#' vector of runtimes to include. Must be some combination of `"r"`, `"python"`,
-#' `"quarto"`, and `"tensorflow"`. Quarto is only supported on Connect >=
-#' 2021.08.0, and Tensorflow is only supported on Connect >= 2024.03.0.
-#' 
+#' @param client A `Connect` object.
+#' @param runtimes Optional. A character vector of runtimes to include. Must be
+#' some combination of `"r"`, `"python"`, `"quarto"`, and `"tensorflow"`. Quarto
+#' is only supported on Connect >= 2021.08.0, and Tensorflow is only supported
+#' on Connect >= 2024.03.0.
+
 #' @return A tibble with columns for `runtime`, `version`, and `cluster_name`
 #' and `image_name`. Cluster name and image name are only meaningful on Connect
 #' instances running off-host execution.
